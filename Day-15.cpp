2cf4f19477ffbbfab0364050307fe0282ea896f8
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-09-16 10:06:50 +0530

    Added Day-15 Solution

--- Day-15.cpp
+++ Day-15.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int lastIndex;
+        for(int i=s.size()-1; i>=0; i--)
+            if(s[i]!=' ')
+            {
+                lastIndex = i;
+                break;
+            }
+        
+        string temp = "";
+        for(int i=0; i<=lastIndex; i++)
+        {
+            if(s[i]==' ')
+                temp = "";
+            else
+                temp += s[i];
+        }
+        return temp.size();
+    }
+};
