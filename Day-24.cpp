88ec6958f71f4bf8a0fb2757192d13c0607ea0f0
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-09-25 10:38:48 +0530

    Added Day-24 Solution

---
+++ Day-24.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for(int i=0; i<s.size(); i++)
+        {
+            sum ^= s[i];
+            sum ^= t[i];
+        }
+        sum ^= t.back();
+        return sum;
+    }
+};
