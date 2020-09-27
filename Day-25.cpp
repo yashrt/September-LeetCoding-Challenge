e10c6ae4fd8163665edb8b6f19d247a2c8da16a7
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-09-27 12:42:20 +0530

    Added Day-25 Solution

--- Day-25.cpp
+++ Day-25.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    static bool comp(int a, int b)
+    {
+        string strA = to_string(a);
+        string strB = to_string(b);
+        return  (strA+strB)>(strB+strA);
+    }
+    
+    string largestNumber(vector<int>& nums) {
+        string res = "";
+        sort(nums.begin(), nums.end(), comp);
+        for(int num: nums)
+            res += to_string(num);
+        
+        return (res[0]!='0') ? res : "0";
+    }
+};
